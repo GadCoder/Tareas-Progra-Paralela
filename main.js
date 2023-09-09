bce1ab955123ce1f704665309f843cbe52b50410
german <german.ampuero@unmsm.edu.pe>
2023-09-09 14:09:31 -0500

    adding example

--- main.js
+++ main.js
@@ -66,9 +66,48 @@ function AJAXpokemon() {
   // Send the request
   xhr.send();
 }
-//Llamar a la funcion
-document
-  .getElementById("obtenerPokemon")
-  .addEventListener("click", function () {
-    AJAXpokemon();
-  });
+
+function setInnerText(elementID, innerText) {
+  const element = document.getElementById(elementID);
+  element.innerText = innerText;
+}
+
+function AJAXstarwars() {
+  // Create a new XMLHttpRequest object
+  const id = getRandomID();
+  const url = "https://swapi.dev/api/people/" + id;
+  //console.log(url);
+  var xhr = new XMLHttpRequest();
+
+  // Configure the request (GET request to a JSON endpoint)
+  xhr.open("GET", url, true);
+
+  // Define the callback function to handle the response
+  xhr.onload = function () {
+    if (xhr.status === 200) {
+      // Parse the JSON response
+      var responseData = JSON.parse(xhr.responseText);
+      console.log(responseData);
+      setInnerText("nombreStarWars", responseData["name"]);
+      setInnerText("alturaSW", responseData["height"]);
+      setInnerText("pesoSW", responseData["mass"]);
+
+      document.getElementById("expPokemon").innerText =
+        responseData.base_experience;
+      document.getElementById("imgPokemon").src =
+        responseData["sprites"]["other"]["official-artwork"]["front_shiny"];
+
+      // Acá se asignaría la información al HTML
+    } else {
+      console.error("Request failed with status:", xhr.status);
+    }
+  };
+
+  // Send the request
+  xhr.send();
+}
+
+document.getElementById("obtenerPersonajes").addEventListener("click", () => {
+  AJAXstarwars();
+  AJAXpokemon();
+});
