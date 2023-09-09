cf00a3736228f428d283d6af2cb8b6a396eb583f
german <german.ampuero@unmsm.edu.pe>
2023-09-09 13:07:54 -0500

    adding example

--- main.js
+++ main.js
@@ -7,6 +7,33 @@ function getRandomID() {
 function AJAXexample() {
   // Create a new XMLHttpRequest object
   const id = getRandomID();
+  const url = "https://swapi.dev/api/people/" + id;
+  console.log(url);
+  var xhr = new XMLHttpRequest();
+
+  // Configure the request (GET request to a JSON endpoint)
+  xhr.open("GET", url, true);
+
+  // Define the callback function to handle the response
+  xhr.onload = function () {
+    if (xhr.status === 200) {
+      // Parse the JSON response
+      var responseData = JSON.parse(xhr.responseText);
+      console.log(responseData);
+
+      // Acá se asignaría la información al HTML
+    } else {
+      console.error("Request failed with status:", xhr.status);
+    }
+  };
+
+  // Send the request
+  xhr.send();
+}
+
+function AJAXpokemon() {
+  // Create a new XMLHttpRequest object
+  const id = getRandomID();
   const url = "https://pokeapi.co/api/v2/pokemon/" + id;
   //console.log(url);
   var xhr = new XMLHttpRequest();
@@ -21,10 +48,14 @@ function AJAXexample() {
       var responseData = JSON.parse(xhr.responseText);
       console.log(responseData); //Todos los datos en la consola
       //console.log(responseData.abilities[0].ability.name); //Nombre y Habilidades
-      document.getElementById("nombrePokemon").innerText=responseData.name.toUpperCase()
-      document.getElementById("habilidadPokemon").innerText=responseData.abilities[0].ability.name.toUpperCase()
-      document.getElementById("expPokemon").innerText=responseData.base_experience
-      document.getElementById("imgPokemon").src=responseData['sprites']['other']['official-artwork']['front_shiny']
+      document.getElementById("nombrePokemon").innerText =
+        responseData.name.toUpperCase();
+      document.getElementById("habilidadPokemon").innerText =
+        responseData.abilities[0].ability.name.toUpperCase();
+      document.getElementById("expPokemon").innerText =
+        responseData.base_experience;
+      document.getElementById("imgPokemon").src =
+        responseData["sprites"]["other"]["official-artwork"]["front_shiny"];
 
       // Acá se asignaría la información al HTML
     } else {
@@ -36,8 +67,8 @@ function AJAXexample() {
   xhr.send();
 }
 //Llamar a la funcion
-document.getElementById("obtenerPokemon").addEventListener('click', function(){
-  AJAXexample();
-})
-
-AJAXexample();
+document
+  .getElementById("obtenerPokemon")
+  .addEventListener("click", function () {
+    AJAXpokemon();
+  });
