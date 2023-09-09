55b4b39d2db8518a191ca16bec02c2b6fabe332c
german <german.ampuero@unmsm.edu.pe>
2023-09-09 14:36:32 -0500

    adding naruto

--- main.js
+++ main.js
@@ -31,6 +31,11 @@ function AJAXexample() {
   xhr.send();
 }
 
+function setInnerText(elementID, innerText) {
+  const element = document.getElementById(elementID);
+  element.innerText = innerText;
+}
+
 function AJAXpokemon() {
   // Create a new XMLHttpRequest object
   const id = getRandomID();
@@ -46,50 +51,17 @@ function AJAXpokemon() {
     if (xhr.status === 200) {
       // Parse the JSON response
       var responseData = JSON.parse(xhr.responseText);
-      console.log(responseData); //Todos los datos en la consola
       //console.log(responseData.abilities[0].ability.name); //Nombre y Habilidades
-      document.getElementById("nombrePokemon").innerText =
-        responseData.name.toUpperCase();
-      document.getElementById("habilidadPokemon").innerText =
-        responseData.abilities[0].ability.name.toUpperCase();
-      document.getElementById("expPokemon").innerText =
-        responseData.base_experience;
+      setInnerText("nombrePokemon", responseData.name.toUpperCase());
+      setInnerText(
+        "habilidadPokemon",
+        responseData.abilities[0].ability.name.toUpperCase()
+      );
+      setInnerText("expPokemon", responseData.base_experience);
       document.getElementById("imgPokemon").src =
         responseData["sprites"]["other"]["official-artwork"]["front_shiny"];
 
       // Acá se asignaría la información al HTML
-    } else {
-      console.error("Request failed with status:", xhr.status);
-    }
-  };
-
-  // Send the request
-  xhr.send();
-}
-
-function setInnerText(elementID, innerText) {
-  const element = document.getElementById(elementID);
-  element.innerText = innerText;
-}
-
-function AJAXstarwars() {
-  // Create a new XMLHttpRequest object
-  const id = getRandomID();
-  const url = "https://swapi.dev/api/people/" + id;
-
-  var xhr = new XMLHttpRequest();
-
-  // Configure the request (GET request to a JSON endpoint)
-  xhr.open("GET", url, true);
-
-  xhr.onload = function () {
-    if (xhr.status === 200) {
-      // Parse the JSON response
-      var responseData = JSON.parse(xhr.responseText);
-      console.log(responseData);
-      setInnerText("nombreStarWars", responseData["name"]);
-      setInnerText("alturaSW", responseData["height"]);
-      setInnerText("pesoSW", responseData["mass"]);
     } else {
       console.error("Request failed with status:", xhr.status);
     }
@@ -110,11 +82,9 @@ function AJAXdigimon() {
 
   // Define the callback function to handle the response
   xhr.onload = function () {
-    console.log(xhr.status);
     if (xhr.status === 200) {
       // Parse the JSON response
       var responseData = JSON.parse(xhr.responseText);
-      console.log(responseData.name);
       document.getElementById("nombreDigimon").innerText = responseData.name;
       document.getElementById("tipoDigimon").innerText =
         responseData.types[0].type;
@@ -133,8 +103,32 @@ function AJAXdigimon() {
   xhr.send();
 }
 
+function AJAXnaruto() {
+  // Create a new XMLHttpRequest object
+  const id = getRandomID();
+  const url = "https://www.narutodb.xyz/api/character/" + id;
+  console.log(url);
+  var xhr = new XMLHttpRequest();
+
+  // Configure the request (GET request to a JSON endpoint)
+  xhr.open("GET", url, true);
+
+  // Define the callback function to handle the response
+  xhr.onload = function () {
+    if (xhr.status === 200) {
+      var responseData = JSON.parse(xhr.responseText);
+      console.log(responseData);
+      setInnerText("nombreNaruto", responseData.name);
+    } else {
+      console.error("Request failed with status:", xhr.status);
+    }
+  };
+  // Send the request
+  xhr.send();
+}
+
 document.getElementById("obtenerPersonajes").addEventListener("click", () => {
-  AJAXstarwars();
   AJAXpokemon();
   AJAXdigimon();
+  AJAXnaruto();
 });
