67ad9b8bbbeeff8972458bf1bd9b5ab67a272b5b
LKA102 <stefano.hernandez@unmsm.edu.pe>
2023-10-04 19:24:21 -0500

    Estableciendo estructura ejercicios parcial

---
+++ EjerciciosParcial/Ej2.py
@@ -0,0 +1,2 @@
+#2. Implementa un programa en Python que realice el 
+# procesamiento paralelo de una imagen utilizando Paralelismo de Datos. 
---
+++ EjerciciosParcial/Ej3.py
@@ -0,0 +1,4 @@
+#3. Implementa un programa en C que realice el 
+# procesamiento paralelo de una imagen utilizando Paralelismo de Tareas. 
+# Divide la imagen en tareas y permite que cada tarea aplique un 
+# filtro o manipulación a su sección antes de combinar los resultados. 
